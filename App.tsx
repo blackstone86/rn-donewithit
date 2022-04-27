14f2dba20835adfccee2167fce8b627494f36484
jimhuang <jimhuang@digitalgd.com.cn>
2022-04-27 09:58:31 +0800

    feat: 🎸 first commit

--- App.tsx
+++ App.tsx
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+// IOS：View -> UIView 原生对象
 export default function App() {
+  // let x = 1;
+  console.log("App executed")
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Hello React Native</Text>
       <StatusBar style="auto" />
     </View>
   );
