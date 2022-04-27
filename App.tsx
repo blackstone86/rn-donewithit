c56c2e504d2bac5ee773754c390b4dd61955464b
jimhuang <jimhuang@digitalgd.com.cn>
2022-04-27 17:48:43 +0800

    fix: 🐛 [App.tsx] addTouchable

--- App.tsx
+++ App.tsx
@@ -6,33 +6,57 @@ import {
   Image,
   Platform,
   SafeAreaView,
+  TouchableWithoutFeedback,
+  TouchableOpacity,
+  TouchableHighlight,
+  TouchableNativeFeedback,
   ActivityIndicator
 } from 'react-native'
+import React, { useState } from 'react'
 import Constants from 'expo-constants'
 const STATUSBAR_HEIGHT = Constants.statusBarHeight
 
 // IOS：View -> UIView 原生对象ssss
 export default function App() {
-  const handlePress = () => console.log('Text pressed')
+  const handleTextPress = () => console.log('Text tapped')
+  const handleImagePress = () => console.log('Image tapped')
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar style="auto" />
-      <Text numberOfLines={1} onPress={handlePress}>
-        Hello React Native Hello React Native Hello React Native Hello React
-        Native Hello React Native Hello React Native Hello React Native Hello
-        React Native Hello React Native
+      <Text numberOfLines={1} onPress={handleTextPress}>
+        Hello React Native
       </Text>
-      <Image style={styles.icon} source={require('./assets/icon.png')} />
-      <Image
-        blurRadius={10}
-        fadeDuration={1000}
-        loadingIndicatorSource={require('./assets/preview.gif')}
-        source={{
-          width: 200,
-          height: 300,
-          uri: 'https://picsum.photos/2000/3000'
-        }}
-      />
+      {/* 点击没有视觉反馈效果 */}
+      <TouchableNativeFeedback
+        onPress={handleImagePress}
+        background={TouchableNativeFeedback.Ripple(
+          'rgba(255, 255, 255, 0.2)',
+          false
+        )}
+      >
+        <View style={styles.view}></View>
+      </TouchableNativeFeedback>
+      {/* 点击背景有蒙层效果 */}
+      <TouchableHighlight onPress={handleImagePress}>
+        <Image style={styles.icon} source={require('./assets/icon.png')} />
+      </TouchableHighlight>
+      {/* 点击有渐变效果 */}
+      <TouchableOpacity onPress={handleImagePress}>
+        <Image style={styles.icon} source={require('./assets/icon.png')} />
+      </TouchableOpacity>
+      {/* 点击没有视觉反馈效果 */}
+      <TouchableWithoutFeedback onPress={handleImagePress}>
+        <Image
+          blurRadius={10}
+          fadeDuration={1000}
+          loadingIndicatorSource={require('./assets/preview.gif')}
+          style={styles.icon}
+          resizeMode="contain"
+          source={{
+            uri: 'https://picsum.photos/200/200'
+          }}
+        />
+      </TouchableWithoutFeedback>
     </SafeAreaView>
   )
 }
@@ -47,7 +71,13 @@ const styles = StyleSheet.create({
     justifyContent: 'center'
   },
   icon: {
-    width: 200,
-    height: 200
+    width: 100,
+    height: 100,
+    backgroundColor: 'dodgerblue'
+  },
+  view: {
+    width: 100,
+    height: 80,
+    backgroundColor: 'dodgerblue'
   }
 })
