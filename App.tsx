786fbea37594a05b96d462b88d8a06afe7d7c71b
jimhuang <jimhuang@digitalgd.com.cn>
2022-04-27 11:26:45 +0800

    fix: 🐛 [App.tsx] compatible safeAreaView in android

--- App.tsx
+++ App.tsx
@@ -1,24 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Platform, SafeAreaView } from 'react-native';
+import Constants from 'expo-constants';
+const STATUSBAR_HEIGHT = Constants.statusBarHeight;
 
 // IOS：View -> UIView 原生对象
 export default function App() {
   // let x = 1;
   console.log("App executed")
   return (
-    <View style={styles.container}>
-      <Text>Hello React Native</Text>
+    <SafeAreaView style={styles.container}>
       <StatusBar style="auto" />
-    </View>
+      <Text>Hello React Native</Text>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'dodgerblue',
+    paddingTop: Platform.OS === 'android' ? STATUSBAR_HEIGHT : 0,
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
 });
