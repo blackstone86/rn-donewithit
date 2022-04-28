781efc6868819ff48f56047df5c44492bd812b70
jimhuang <jimhuang@digitalgd.com.cn>
2022-04-28 20:36:30 +0800

    feat: 🎸 [App.tsx] add Flex layout

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 // import App from './demos/BestPractice'
-import App from './demos/useDimensions'
-// import App from './demos/useDeviceOrientation'
+import App from './demos/UseDimensions'
+// import App from './demos/UseDeviceOrientation'
 
 /**
  * notice
  * 1.BestPractices 代码推荐写法
  * 2.Dimensions 尺寸
+ * 3.Orientation 旋转
+ * 4.Flex 布局
  */
 export default App;
