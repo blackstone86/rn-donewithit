109494604ad61c0b0ed65b51ce72d62bccf1a209
jimhuang <jimhuang@digitalgd.com.cn>
2022-04-28 22:30:42 +0800

    feat: 🎸 [FlexWrap.jsx] add flexwrap

--- demos/FlexWrap.jsx
+++ demos/FlexWrap.jsx
@@ -5,18 +5,26 @@ import React from 'react'
 
 export default function App() {
 
+  /**
+   * flexWrap available values
+   * nowrap
+   * wrap
+   * wrap-reverse
+   */
   return (
     <View style={{
       backgroundColor: '#fff',
       flex: 1,
       flexDirection: 'row', // default: column
       justifyContent: 'center', // main axis
-      alignItems: 'center', // secondary axis
+      alignItems: 'center', // secondary axis , control items within each line.
+      alignContent: 'center', // secondary axis, alignment of the entire content
+      flexWrap: 'wrap'
     }}>
       <View style={{
         backgroundColor: 'dodgerblue',
         width: 100,
-        height: 100,
+        height: 300,
       }} />
       <View style={{
         backgroundColor: 'gold',
@@ -25,6 +33,16 @@ export default function App() {
       }} />
       <View style={{
         backgroundColor: 'tomato',
+        width: 100,
+        height: 100,
+      }} />
+      <View style={{
+        backgroundColor: 'grey',
+        width: 100,
+        height: 100,
+      }} />
+      <View style={{
+        backgroundColor: 'greenyellow',
         width: 100,
         height: 100,
       }} />
