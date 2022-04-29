18d364c0a2fd75f77dcc23984ef10486a5edf57e
jimhuang <jimhuang@digitalgd.com.cn>
2022-04-29 14:13:56 +0800

    feat: 🎸 [demos/Exercises.jsx] add login page

--- App.tsx
+++ App.tsx
@@ -5,7 +5,9 @@
 // import App from './demos/FlexDirection'
 // import App from './demos/FlexAlign'
 // import App from './demos/FlexWrap'
-import App from './demos/FlexBasisGrowShrink'
+// import App from './demos/FlexBasisGrowShrink'
+// import App from './demos/Absolute&Relative'
+import App from './demos/Exercises'
 
 /**
  * notice
@@ -16,5 +18,7 @@ import App from './demos/FlexBasisGrowShrink'
  * 5.FlexDirection 布局主轴方向
  * 6.FlexAlign 对齐方式
  * 7.FlexBasisGrowShrink 自动占满主轴或在主轴按比例收缩显示
+ * 8.Absolute&Relative 相对定位，绝对定位
+ * 9.Exercises 练习
  */
 export default App;
---
+++ demos/Absolute&Relative.jsx
@@ -0,0 +1,42 @@
+import {
+  View
+} from 'react-native'
+import React from 'react'
+
+export default function App() {
+  /**
+   * position
+   * 1.default relative
+   * 2.absolute detach doc flow, but does not change the z-index
+   */
+  return (
+    <View style={{
+      backgroundColor: '#fff',
+      flex: 1,
+      flexDirection: 'row', // default: column
+      justifyContent: 'center', // main axis
+      alignItems: 'center', // secondary axis , control items within each line.
+    }}>
+      <View>
+        <View style={{
+          backgroundColor: 'dodgerblue',
+          width: 100,
+          height: 100,
+        }} />
+        <View style={{
+          backgroundColor: 'gold',
+          width: 100,
+          height: 100,
+          top: 20,
+          left: 20,
+          position: 'absolute'
+        }} />
+        <View style={{
+          backgroundColor: 'tomato',
+          width: 100,
+          height: 100,
+        }} />
+      </View>
+    </View>
+  )
+}
