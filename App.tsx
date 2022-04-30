7881da86e834481fb4dd8eca1101324179dcb1a4
jimhuang <jimhuang@digitalgd.com.cn>
2022-04-30 16:40:59 +0800

    feat: 🎸 [demos/Shadows.jsx] add shadow usage

--- App.tsx
+++ App.tsx
@@ -9,18 +9,22 @@
 // import App from './demos/Absolute&Relative'
 // import App from './demos/Exercises01'
 // import App from './demos/Exercises02'
-import App from './demos/Borders'
+// import App from './demos/Borders'
+import App from './demos/Shadows'
 
 /**
  * notice
- * 1.BestPractices 代码推荐写法
- * 2.Dimensions 尺寸
- * 3.Orientation 旋转
- * 4.Flexbox 布局
- * 5.FlexDirection 布局主轴方向
- * 6.FlexAlign 对齐方式
- * 7.FlexBasisGrowShrink 自动占满主轴或在主轴按比例收缩显示
- * 8.Absolute&Relative 相对定位，绝对定位
- * 9.Exercises01 练习启动页
+ * 01.BestPractices 代码推荐写法
+ * 02.Dimensions 尺寸
+ * 03.Orientation 旋转
+ * 04.Flexbox 布局
+ * 05.FlexDirection 布局主轴方向
+ * 06.FlexAlign 对齐方式
+ * 07.FlexBasisGrowShrink 自动占满主轴或在主轴按比例收缩显示
+ * 08.Absolute&Relative 相对定位，绝对定位
+ * 09.Exercises01 练习启动页
+ * 10.Exercises02 图片与按钮布局
+ * 11.Borders 边框
+ * 12.Shadows 阴影
  */
 export default App;
