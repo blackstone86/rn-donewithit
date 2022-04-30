c50eb033843c92f9dd3229b282075ffbca96bf0b
jimhuang <jimhuang@digitalgd.com.cn>
2022-04-30 21:17:08 +0800

    feat: 🎸 [demos/Paddings&Margins.jsx] add paddings and margins

--- App.tsx
+++ App.tsx
@@ -10,7 +10,8 @@
 // import App from './demos/Exercises01'
 // import App from './demos/Exercises02'
 // import App from './demos/Borders'
-import App from './demos/Shadows'
+// import App from './demos/Shadows'
+import App from './demos/Paddings&Margins'
 
 /**
  * notice
@@ -20,11 +21,13 @@ import App from './demos/Shadows'
  * 04.Flexbox 布局
  * 05.FlexDirection 布局主轴方向
  * 06.FlexAlign 对齐方式
- * 07.FlexBasisGrowShrink 自动占满主轴或在主轴按比例收缩显示
- * 08.Absolute&Relative 相对定位，绝对定位
- * 09.Exercises01 练习启动页
- * 10.Exercises02 图片与按钮布局
- * 11.Borders 边框
- * 12.Shadows 阴影
+ * 07.FlexWrap 完整显示超出屏幕内容
+ * 08.FlexBasisGrowShrink 自动占满主轴或在主轴按比例收缩显示
+ * 09.Absolute&Relative 相对定位，绝对定位
+ * 10.Exercises01 练习启动页
+ * 11.Exercises02 图片与按钮布局
+ * 12.Borders 边框
+ * 13.Shadows 阴影
+ * 14.Padding&Margins 内边距，外边距差异
  */
 export default App;
---
+++ demos/Paddings&Margins.jsx
@@ -0,0 +1,67 @@
+import {
+  View
+} from 'react-native'
+import React from 'react'
+
+export default function App() {
+  /**
+   * padding
+   * 1.paddingHorizontal: set paddingLeft and paddingRight = x
+   * 2.paddingVertical: set paddingTop and paddingBottom = y
+   * 3-1.priority: paddingLeft,paddingRight > paddingHorizontal > padding
+   * 3-2.priority: paddingTop,paddingBottom > paddingVertical > padding
+   */
+  return (
+    <View style={{
+      backgroundColor: '#fff',
+      flex: 1,
+      flexDirection: 'column', // default: column
+      justifyContent: 'center', // main axis
+      alignItems: 'center', // secondary axis , control items within each line.
+    }}>
+      <View style={{
+        backgroundColor: 'dodgerblue',
+        width: 200,
+        height: 200,
+
+        padding: 20,
+
+        paddingHorizontal: 20,
+        paddingLeft: 20,
+        paddingRight: 20,
+
+        paddingVertical: 20,
+        paddingTop: 20,
+        paddingBottom: 20,
+      }}>
+        <View style={{
+          backgroundColor: 'yellow',
+          flex: 1,
+        }} />
+      </View>
+
+      <View style={{
+        width: 200,
+        height: 200,
+        backgroundColor: 'tomato',
+        margin: 20,
+      }}>
+        <View style={{
+          width: 100,
+          height: 100,
+          backgroundColor: 'dodgerblue',
+          margin: 30,
+
+          marginHorizontal: 30,
+          marginLeft: 30,
+          marginRight: 30,
+
+          marginVertical: 30,
+          marginTop: 30,
+          marginBottom: 30,
+        }}>
+        </View>
+      </View>
+    </View>
+  )
+}
