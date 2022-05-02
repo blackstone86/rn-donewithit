698297a6ddbcbc3212307e17e2a803d1fffe08d0
jimhuang <jimhuang@digitalgd.com.cn>
2022-05-02 22:46:09 +0800

    feat: 🎸 [AppFlatList.tsx] encapsulate FlatList to AppFlatList

---
+++ components/AppFlatList/AppFlatList.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { FlatList, View, StyleSheet, FlatListProps } from 'react-native'
+import COLORS from '../../config/colors'
+
+const styles = StyleSheet.create({
+  separator: {
+    width: '100%',
+    height: 1,
+    backgroundColor: COLORS.GRAY
+  }
+})
+
+function AppFlatList(props: FlatListProps<any>) {
+  return (
+    <FlatList
+      {...props}
+      ItemSeparatorComponent={() => <View style={styles.separator} />}
+    ></FlatList>
+  )
+}
+
+export default AppFlatList
