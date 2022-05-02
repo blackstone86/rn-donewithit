62c947e43d8b3e7cbcb17337f13a664f569d1974
jimhuang <jimhuang@digitalgd.com.cn>
2022-05-02 22:11:34 +0800

    fix: 🐛 [FlatList.tsx] clean code

--- demos/FlatList.tsx
+++ demos/FlatList.tsx
@@ -1,4 +1,4 @@
-import { FlatList, SafeAreaView, StyleSheet, Platform } from 'react-native'
+import { FlatList } from 'react-native'
 
 import React from 'react'
 import AppListItem from '../components/AppListItem'
