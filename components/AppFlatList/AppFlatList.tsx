1c17cf88e2648b38668ea837ef95595f732322e2
jimhuang <jimhuang@digitalgd.com.cn>
2022-05-02 22:54:14 +0800

    fix: 🐛 [colors.ts] add LIGHT_GRAY

--- components/AppFlatList/AppFlatList.tsx
+++ components/AppFlatList/AppFlatList.tsx
@@ -6,7 +6,7 @@ const styles = StyleSheet.create({
   separator: {
     width: '100%',
     height: 1,
-    backgroundColor: COLORS.GRAY
+    backgroundColor: COLORS.LIGHT_GRAY
   }
 })
 
--- config/colors.ts
+++ config/colors.ts
@@ -3,6 +3,7 @@ const COLORS = {
   SECONDARY: 'turquoise',
   BACKGROUND_COLOR: '#f8f4f4',
   GRAY: '#6e6969',
+  LIGHT_GRAY: '#f8f4f4',
   WHITE: '#fff',
   BLACK: '#000',
   SILVER: '#C0C0C0'
