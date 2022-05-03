81f7e29123bc00db4ea1908a57c678488ec804a2
jimhuang <jimhuang@digitalgd.com.cn>
2022-05-03 21:55:20 +0800

    feat: 🎸 [AppListItemDeleteAction.tsx] add onDelete callback

--- components/AppListItemDeleteAction/AppListItemDeleteAction.tsx
+++ components/AppListItemDeleteAction/AppListItemDeleteAction.tsx
@@ -1,25 +1,34 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import { StyleSheet } from 'react-native'
 import { View } from 'react-native'
 import COLORS from '../../config/colors'
 import CONSTS from './consts'
 import { MaterialCommunityIcons } from '@expo/vector-icons'
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler'
+import { AppListItemDeleteActionProps } from './types'
 
-function AppListItemDeleteAction(props: any) {
+function AppListItemDeleteAction({ onDelete }: AppListItemDeleteActionProps) {
+  const handlePress = useCallback(() => {
+    typeof onDelete === 'function' && onDelete()
+  }, [])
+
   return (
-    <View style={styles.container}>
-      <MaterialCommunityIcons
-        name="trash-can"
-        size={CONSTS.ICON_SIZE}
-        color={COLORS.WHITE}
-      />
-    </View>
+    <TouchableWithoutFeedback onPress={handlePress}>
+      <View style={styles.container}>
+        <MaterialCommunityIcons
+          name="trash-can"
+          size={CONSTS.ICON_SIZE}
+          color={COLORS.WHITE}
+        />
+      </View>
+    </TouchableWithoutFeedback>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
     width: 80,
+    height: '100%',
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: COLORS.DANGER
