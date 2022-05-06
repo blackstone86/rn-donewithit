836277bdc49366d9f21ac3561c60a5e63067a554
jimhuang <jimhuang@digitalgd.com.cn>
2022-05-06 09:26:07 +0800

    fix: 🐛 [AppButton.tsx] fix onPress callback
    fix onPress callback can not get state params.

--- components/AppButton/AppButton.tsx
+++ components/AppButton/AppButton.tsx
@@ -1,10 +1,9 @@
-import React, { useCallback } from 'react'
+import React from 'react'
 import {
   View,
   TouchableWithoutFeedback,
   TouchableNativeFeedback,
-  Platform,
-  GestureResponderEvent
+  Platform
 } from 'react-native'
 import AppText from '../AppText'
 import useStyles from './useStyles'
@@ -19,9 +18,9 @@ function AppButton({
   style,
   onPress
 }: AppButtonProps) {
-  const handlePress = useCallback((e: GestureResponderEvent) => {
-    typeof onPress === 'function' && onPress(e)
-  }, [])
+  // const handlePress = useCallback((e: GestureResponderEvent) => { // not recommend!
+  //   typeof onPress === 'function' && onPress(e)
+  // }, [])
   const styles = useStyles({ fontSize, height, backgroundColor, borderRadius })
   const button = (
     <View style={styles.button}>
@@ -32,11 +31,11 @@ function AppButton({
   return (
     <View style={[styles.buttonBox, style]}>
       {isAndroid ? (
-        <TouchableNativeFeedback onPress={handlePress}>
+        <TouchableNativeFeedback onPress={onPress}>
           {button}
         </TouchableNativeFeedback>
       ) : (
-        <TouchableWithoutFeedback onPress={handlePress}>
+        <TouchableWithoutFeedback onPress={onPress}>
           {button}
         </TouchableWithoutFeedback>
       )}
