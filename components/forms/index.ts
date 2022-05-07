e664fd8bb2c5983c66d27725744b1b7d74761372
jimhuang <jimhuang@digitalgd.com.cn>
2022-05-07 15:52:21 +0800

    feat: 🎸 [forms/index.ts] improve syntax with export

--- components/forms/index.ts
+++ components/forms/index.ts
@@ -1,6 +1,11 @@
-import AppForm from './AppForm'
-import AppFormField, { Field } from './AppFormField'
-import AppErrorMessage from './AppErrorMessage'
-import AppSubmitButton from './AppSubmitButton'
+// import AppForm from './AppForm'
+// import AppFormField, { Field } from './AppFormField'
+// import AppErrorMessage from './AppErrorMessage'
+// import AppSubmitButton from './AppSubmitButton'
+// export { AppForm, AppFormField, AppErrorMessage, AppSubmitButton, Field }
 
-export { AppForm, AppFormField, AppErrorMessage, AppSubmitButton, Field }
+// recommend
+export { default as AppForm } from './AppForm'
+export { default as AppFormField, Field } from './AppFormField'
+export { default as AppErrorMessage } from './AppErrorMessage'
+export { default as AppSubmitButton } from './AppSubmitButton'
