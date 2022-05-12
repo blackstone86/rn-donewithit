9e27a655f4cf9d972b7ddf99fa7fd030fd6667af
jimhuang <jimhuang@digitalgd.com.cn>
2022-05-12 17:46:59 +0800

    feat: 🎸 [useLocation.tsx] add try,catch

--- app/hooks/useLocation.ts
+++ app/hooks/useLocation.ts
@@ -13,16 +13,20 @@ const useLocation = (): LocationType => {
   }
   const [location, setLocation] = useState<LocationType>(initLocation)
   const getLocation = async () => {
-    const { granted } = await Location.requestForegroundPermissionsAsync()
-    if (!granted) return
-    // let {
-    //   coords: { latitude, longitude }
-    // } = await Location.getCurrentPositionAsync({
-    //   accuracy: Location.Accuracy.Highest // 模拟器可以，小米11 Ultra失败
-    // })
-    let pos = await Location.getLastKnownPositionAsync()
-    let { latitude, longitude } = pos?.coords || initLocation
-    setLocation({ latitude, longitude })
+    try {
+      const { granted } = await Location.requestForegroundPermissionsAsync()
+      if (!granted) return
+      // let {
+      //   coords: { latitude, longitude }
+      // } = await Location.getCurrentPositionAsync({
+      //   accuracy: Location.Accuracy.Highest // 模拟器可以，小米11 Ultra失败
+      // })
+      let pos = await Location.getLastKnownPositionAsync()
+      let { latitude, longitude } = pos?.coords || initLocation
+      setLocation({ latitude, longitude })
+    } catch (error) {
+      console.log('Error reading a location', error)
+    }
   }
   useEffect(() => {
     getLocation()
