afb41730c21948aca65adee6cab71ce518700c3c
jimhuang <jimhuang@digitalgd.com.cn>
2022-05-13 10:56:21 +0800

    feat: 🎸 [NativeStackScreen.tsx] add hook

--- app/demos/NativeStackScreen.tsx
+++ app/demos/NativeStackScreen.tsx
@@ -1,36 +1,107 @@
 import React from 'react'
 import AppSafeAreaView from '../components/AppSafeAreaView'
 import AppText from '../components/AppText'
-import { NavigationContainer } from '@react-navigation/native'
+import { NavigationContainer, useNavigation } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
-const Stack = createNativeStackNavigator()
+import AppButton from '../components/AppButton'
+import { StyleProp, ViewStyle, StyleSheet } from 'react-native'
 
-function Tweets() {
+const Stack = createNativeStackNavigator()
+const styles = StyleSheet.create({
+  button: {
+    marginHorizontal: 10,
+    marginBottom: 10
+  },
+  text: {
+    textAlign: 'center',
+    lineHeight: 30,
+    fontSize: 20,
+    marginBottom: 10
+  }
+})
+type LinkType = {
+  title?: string
+  targetScreenName?: any
+  style?: StyleProp<ViewStyle>
+}
+enum SCREEN {
+  Tweets = 'Tweets',
+  TweetDetails = 'TweetDetails',
+  TweetInfos = 'TweetInfos'
+}
+function Link({
+  title = 'Click',
+  targetScreenName = SCREEN.Tweets,
+  style
+}: LinkType) {
+  const navigation = useNavigation()
+  return (
+    <AppButton
+      style={style}
+      title={title}
+      onPress={() => {
+        navigation.navigate(targetScreenName)
+      }}
+    />
+  )
+}
+
+function Tweets({ navigation }: any) {
   return (
     <AppSafeAreaView>
-      <AppText>Tweets</AppText>
+      <AppText style={styles.text}>{SCREEN.Tweets}</AppText>
+      <AppButton
+        style={styles.button}
+        title={SCREEN.TweetDetails}
+        onPress={() => {
+          navigation.navigate(SCREEN.TweetDetails)
+          // navigation.navigate(SCREEN.Tweets) // 在当前页不动
+          // navigation.push(SCREEN.Tweets) // 堆栈新增一个实例
+        }}
+      />
+      <Link
+        style={styles.button}
+        title={SCREEN.TweetInfos}
+        targetScreenName={SCREEN.TweetInfos}
+      />
     </AppSafeAreaView>
   )
 }
 
-function TweetDetails() {
+function TweetDetails({ navigation }: any) {
   return (
     <AppSafeAreaView>
-      <AppText>TweetDetails</AppText>
+      <AppText style={styles.text}>{SCREEN.TweetDetails}</AppText>
+      <AppButton
+        style={styles.button}
+        title={SCREEN.Tweets}
+        onPress={() => {
+          navigation.navigate(SCREEN.Tweets)
+        }}
+      />
+    </AppSafeAreaView>
+  )
+}
+
+function TweetInfos({ navigation }: any) {
+  return (
+    <AppSafeAreaView>
+      <AppText style={styles.text}>{SCREEN.TweetInfos}</AppText>
     </AppSafeAreaView>
   )
 }
 
 function Navigator() {
   return (
-    <Stack.Navigator initialRouteName="Tweets">
-      <Stack.Screen name="Tweets" component={Tweets} />
-      <Stack.Screen name="TweetDetails" component={TweetDetails} />
+    <Stack.Navigator initialRouteName={SCREEN.Tweets}>
+      <Stack.Screen name={SCREEN.Tweets} component={Tweets} />
+      <Stack.Screen name={SCREEN.TweetDetails} component={TweetDetails} />
+      <Stack.Screen name={SCREEN.TweetInfos} component={TweetInfos} />
     </Stack.Navigator>
   )
 }
 
-function NativeStackScreen(props: any) {
+function NativeStackScreen() {
   return (
     <AppSafeAreaView>
       <NavigationContainer>
