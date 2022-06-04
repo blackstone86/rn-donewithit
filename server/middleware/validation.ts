da9a7ceab63564927e0524851709e2cb9842407f
jimhuang <jimhuang@digitalgd.com.cn>
2022-06-04 14:47:55 +0800

    feat: 🎸 [validation.ts] use joi's new syntax

--- server/middleware/validation.ts
+++ server/middleware/validation.ts
@@ -1,7 +1,8 @@
 import Joi from 'joi'
 
 export default (schema) => (req, res, next) => {
-  const result = Joi.validate(req.body, schema)
+  // const result = Joi.validate(req.body, schema)
+  const result = schema.validate(req.body)
 
   if (result.error)
     return res.status(400).send({ error: result.error.details[0].message })
