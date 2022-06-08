4d892b20a7b1a8abef800c193479b6e1904b6077
jimhuang <jimhuang@digitalgd.com.cn>
2022-06-08 13:35:08 +0800

    feat: 🎸 [screenName.ts] add screenName enum

---
+++ app/navigators/screenNames.ts
@@ -0,0 +1,13 @@
+enum ScreenName {
+  ACCOUNT_SCREEN = 'AccountScreen',
+  LISTING_DETAILS_SCREEN = 'ListingDetailsScreen',
+  LISTING_EDIT_SCREEN = 'ListingEditScreen',
+  LISTINGS_SCREEN = 'ListingsScreen',
+  LOGIN_SCREEN = 'LoginScreen',
+  MESSAGES_SCREEN = 'MessagesScreen',
+  REGISTER_SCREEN = 'RegisterScreen',
+  VIEW_IMAGE_SCREEN = 'ViewImageScreen',
+  WELCOME_SCREEN = 'WelcomeScreen'
+}
+
+export default ScreenName
