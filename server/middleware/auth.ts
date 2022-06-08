9489778a99b31d66200d639ee51a85b610224ad7
jimhuang <jimhuang@digitalgd.com.cn>
2022-06-08 15:53:51 +0800

    feat: 🎸 [auth.ts] use real jwt token

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -1,29 +1,13 @@
 import jwt from 'jsonwebtoken'
 
 export default (req, res, next) => {
-  // const token = req.header('x-auth-token')
-  // if (!token)
-  //   return res.status(401).send({ error: 'Access denied. No token provided.' })
-
-  // try {
-  //   const payload = jwt.verify(token, 'jwtPrivateKey')
-  //   req.user = payload
-  //   next()
-  // } catch (err) {
-  //   res.status(400).send({ error: 'Invalid token.' })
-  // }
-
-  const user = {
-    id: 1,
-    name: 'Mosh',
-    email: 'mosh@domain.com',
-    password: '12345'
-  }
-
-  const token = { userId: user.id, name: user.name, email: user.email }
+  const token = req.header('x-auth-token')
+  if (!token)
+    return res.status(401).send({ error: 'Access denied. No token provided.' })
 
   try {
-    req.user = token
+    const payload = jwt.verify(token, 'jwtPrivateKey')
+    req.user = payload
     next()
   } catch (err) {
     res.status(400).send({ error: 'Invalid token.' })
