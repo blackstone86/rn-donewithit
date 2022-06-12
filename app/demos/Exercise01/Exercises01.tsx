e91369f27db5b10dc54972173c7cc3e145a2d1fb
jimhuang <jimhuang@digitalgd.com.cn>
2022-06-12 09:56:30 +0800

    feat: 🎸 [Exercise01.tsx] toggle AuthNavigator, AppNavigator

--- app/demos/Exercise01/Exercises01.tsx
+++ app/demos/Exercise01/Exercises01.tsx
@@ -1,12 +1,16 @@
 import { NavigationContainer } from '@react-navigation/native'
 import React from 'react'
 import AuthNavigator from '../../navigators/AuthNavigator'
+import AppNavigator from '../../navigators/AppNavigator'
 import navigationTheme from '../../navigators/navigationTheme'
+import { useSelector } from 'react-redux'
+import { RootState } from '../../store'
 
 function Exercises01(props: any) {
+  const LoggedIn = useSelector((state: RootState) => state.auth.LoggedIn)
   return (
     <NavigationContainer theme={navigationTheme}>
-      <AuthNavigator />
+      {LoggedIn ? <AppNavigator /> : <AuthNavigator />}
     </NavigationContainer>
   )
 }
