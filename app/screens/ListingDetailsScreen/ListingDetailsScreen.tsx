75e1bbd5cf239c9203959411d8ff001a05143b45
jimhuang <jimhuang@digitalgd.com.cn>
2022-06-12 09:44:28 +0800

    feat: 🎸 [ListingDetailsScreen.tsx] use api

--- app/screens/ListingDetailsScreen/ListingDetailsScreen.tsx
+++ app/screens/ListingDetailsScreen/ListingDetailsScreen.tsx
@@ -1,44 +1,77 @@
 import { Image, ScrollView, View } from 'react-native'
-import React, { useMemo } from 'react'
+import React, { useEffect } from 'react'
 import AppText from '../../components/AppText'
 import styles from './styles'
-import { JACKET, MOSH } from '../../config/images'
+import { MOSH } from '../../config/images'
+import useApi from '../../hooks/useApi'
+import { listingApi } from '../../api'
+import AppActivityIndicator from '../../components/AppActivityIndicator'
+import AppRetryView from '../../components/AppRetryView'
 
-export default function ListingDetailsScreen({ route: { params } }: any) {
-  const { title, subTitle, image, imageUrl, avatarImage, name, listings } =
-    useMemo(
-      () => ({
-        title: params.title,
-        subTitle: params.subTitle,
-        image: params.image,
-        imageUrl: params.imageUrl,
-        avatarImage: MOSH,
-        name: 'Mosh Hamedani',
-        listings: 5
-      }),
-      []
-    )
+export default function ListingDetailsScreen({
+  route: {
+    params: { id }
+  }
+}: any) {
+  const {
+    data: listing,
+    error,
+    loading,
+    request: setData
+  } = useApi(listingApi.getListing, (data: any) => {
+    // userId: 2,
+    // location: {
+    //   latitude: 37.78825,
+    //   longitude: -122.4324
+    // }
+    return {
+      ...data,
+      subTitle: `$${data.price}`,
+      imageUrl: data.images[0].url,
+      avatarImage: MOSH,
+      name: 'Mosh Hamedani',
+      listings: 5
+    }
+  })
+  useEffect(() => {
+    setData(id)
+  }, [])
+  const { title, subTitle, imageUrl, avatarImage, name, listings } =
+    listing || {}
 
   return (
-    <ScrollView>
-      <View style={styles.container}>
-        <Image style={styles.image} source={{ uri: imageUrl } || image} />
-        <View style={styles.infoBox}>
-          <AppText style={[styles.text, styles.title]}>{title}</AppText>
-          {subTitle && (
-            <AppText style={[styles.text, styles.subTitle]}>{subTitle}</AppText>
-          )}
-        </View>
-        <View style={styles.sellerBox}>
-          <Image style={styles.avatar} source={avatarImage} />
-          <View>
-            <AppText style={styles.sellerText}>{name}</AppText>
-            <AppText
-              style={[styles.sellerText, styles.total]}
-            >{`${listings} Listings`}</AppText>
+    <>
+      {loading && <AppActivityIndicator visible loop />}
+      {error && (
+        <AppRetryView
+          title="Couldn't retrieve the listing"
+          handleRetry={setData}
+        />
+      )}
+      {!loading && !error && (
+        <ScrollView>
+          <View style={styles.container}>
+            <Image style={styles.image} source={{ uri: imageUrl }} />
+            <View style={styles.infoBox}>
+              <AppText style={[styles.text, styles.title]}>{title}</AppText>
+              {subTitle && (
+                <AppText style={[styles.text, styles.subTitle]}>
+                  {subTitle}
+                </AppText>
+              )}
+            </View>
+            <View style={styles.sellerBox}>
+              <Image style={styles.avatar} source={avatarImage} />
+              <View>
+                <AppText style={styles.sellerText}>{name}</AppText>
+                <AppText
+                  style={[styles.sellerText, styles.total]}
+                >{`${listings} Listings`}</AppText>
+              </View>
+            </View>
           </View>
-        </View>
-      </View>
-    </ScrollView>
+        </ScrollView>
+      )}
+    </>
   )
 }
