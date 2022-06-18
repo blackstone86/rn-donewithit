cae42171ff218a551562428fb0be61f008e6b035
jimhuang <jimhuang@digitalgd.com.cn>
2022-06-18 15:30:04 +0800

    feat: 🎸 [config/settings] add app common config

--- app/config/settings.ts
+++ app/config/settings.ts
@@ -1,12 +1,16 @@
 import * as Updates from 'expo-updates'
 
+const APP_COMMON_CONFIG = {
+  PREFIX: 'cache_'
+}
+
 export enum EnvironmentType {
   DEV = 'dev',
   STAGING = 'staging',
   PROD = 'prod'
 }
 
-const settings = {
+const settings: any = {
   dev: {
     // 本地环境
     environment: EnvironmentType.DEV
@@ -20,6 +24,10 @@ const settings = {
     environment: EnvironmentType.PROD
   }
 }
+
+Object.keys(settings).forEach((key) => {
+  Object.assign(settings[key], APP_COMMON_CONFIG)
+})
 
 // https://docs.expo.dev/distribution/release-channels/#example-workflow
 // expo publish --release-channel staging-v1
