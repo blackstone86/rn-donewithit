3b52681525f3486768d807def42650db0eff5df9
jimhuang <jimhuang@digitalgd.com.cn>
2022-06-18 15:31:25 +0800

    feat: 🎸 [Exercise01.tsx] add apploading

--- app/demos/Exercise01/Exercises01.tsx
+++ app/demos/Exercise01/Exercises01.tsx
@@ -1,19 +1,23 @@
 import { NavigationContainer } from '@react-navigation/native'
 // import { useSelector } from 'react-redux'
 // import { RootState } from '../../store'
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { AuthContext } from '../../contexts'
 import AuthNavigator from '../../navigators/AuthNavigator'
 import AppNavigator from '../../navigators/AppNavigator'
 import navigationTheme from '../../navigators/navigationTheme'
 import { useNetInfo } from '@react-native-community/netinfo'
 import AppNoticeText from '../../components/AppNoticeText'
+import AppLoading from 'expo-app-loading'
+import authStorage from '../../utils/authStorage'
+import jwtDecode from 'jwt-decode'
 
 function Exercises01(props: any) {
   // const LoggedIn = useSelector((state: RootState) => state.auth.LoggedIn)
-  const { user } = useContext(AuthContext)
-  const LoggedIn = !!user
+  const { user, setUser } = useContext(AuthContext)
+  const [isReady, setIsReady] = useState<boolean>(false)
   const { isConnected } = useNetInfo()
+
   // Object {
   //   "details": Object {},
   //   "isConnected": false,
@@ -21,6 +25,28 @@ function Exercises01(props: any) {
   //   "isWifiEnabled": false,
   //   "type": "none",
   // }
+
+  const restoreToken = async () => {
+    const token = await authStorage.getToken()
+    if (!token) return
+    if (token) {
+      setUser(jwtDecode(token))
+    }
+  }
+
+  if (!isReady)
+    return (
+      <AppLoading
+        startAsync={restoreToken}
+        onFinish={() => setIsReady(true)}
+        onError={(err) => {
+          console.log(err)
+        }}
+      />
+    )
+
+  const LoggedIn = !!user
+
   return (
     <NavigationContainer theme={navigationTheme}>
       {LoggedIn ? <AppNavigator /> : <AuthNavigator />}
