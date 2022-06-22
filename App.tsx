b9f8dcca74ae3a2c01a337a754a3107c5d43e687
jimhuang <jimhuang@digitalgd.com.cn>
2022-06-22 22:57:40 +0800

    feat: 🎸 [Notifications.tsx] add Notifications, get pushToken

--- App.tsx
+++ App.tsx
@@ -3,49 +3,27 @@ import AuthContext from './app/contexts/auth'
 // import { Provider } from 'react-redux'
 // import { store } from './app/store'
 import logger from './app/utils/logger'
-import Exercise01 from './app/demos/Exercise01'
-// import NetinfoScreen from './app/demos/NetinfoScreen'
-// import AsyncStorageScreen from './app/demos/AsyncStorageScreen'
-// import CacheScreen from './app/demos/CacheScreen'
+// import Exercise01 from './app/demos/Exercise01'
+import Notifications from './app/demos/Notifications'
 
 logger.start()
 
 /**
- * notice
+ * options
  *
- * 1.Offline Strategies
- * 01.notify user with the netInfo
- * 02.disable features
- * 03.cache data
- * 04.store user actions (involve modifying data)
- *   - complexity solution
- *     - realm
- *     - Firebase
- *
- * 2.Caching
- * data is available across restarts but data is wiped out when uninstall the app.
- * 01.asyncStorage : limits 6MB
- *    under the hood:
- *    Android : SQLite
- *    IOS : Dictionary/Files
- * 02.SecureStore : limits 2MB
- * 03.sqlite : database engine
- *
- * 3.Cache listings data with asyncStorage
+ * 1.Expo's Push Notification Service [Expo]
+ * 2.One Signal [One Signal]
+ * 3.Firebase Cloud Messaging [Google]
+ * 4.Amazon's Simple Notification Service (SNS) [Amazon]
  */
 export default function App() {
   const [user, setUser] = useState(null)
   const value = useMemo(() => ({ user, setUser }), [user])
 
   return (
-    <AuthContext.Provider value={value}>
-      <Exercise01 />
-    </AuthContext.Provider>
-    // <Provider store={store}>
+    // <AuthContext.Provider value={value}>
     //   <Exercise01 />
-    // </Provider>
-    // <NetinfoScreen />
-    // <AsyncStorageScreen />
-    // <CacheScreen />
+    // </AuthContext.Provider>
+    <Notifications />
   )
 }
---
+++ app/demos/Notifications.tsx
@@ -0,0 +1,42 @@
+import React, { useEffect } from 'react'
+import AppSafeAreaView from '../components/AppSafeAreaView'
+import AppText from '../components/AppText'
+import * as Notifications from 'expo-notifications'
+
+/**
+ * Steps
+ *
+ * 1. register the app to get a token
+ * 2. store the token on the server
+ * 3. send a notification
+ * 4. handle received notifications
+ */
+
+const getPushToken = async () => {
+  try {
+    const { granted } = await Notifications.getPermissionsAsync()
+    if (!granted) {
+      alert('You need to enable permission to access notifications!')
+    }
+    const { data: pushToken } = await Notifications.getExpoPushTokenAsync()
+    console.log(pushToken)
+  } catch (err) {
+    console.log('Error occurs when getting pushToken', err)
+  }
+}
+
+function NotificationsScreen(props: any) {
+  useEffect(() => {
+    getPushToken()
+  }, [])
+
+  return (
+    <AppSafeAreaView>
+      <AppText style={{ fontSize: 30, textAlign: 'center' }}>
+        Notifications
+      </AppText>
+    </AppSafeAreaView>
+  )
+}
+
+export default NotificationsScreen
