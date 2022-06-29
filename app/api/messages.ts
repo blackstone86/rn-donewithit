f66c3c6ae6a686fea18338f39318a05fa535017b
jimhuang <jimhuang@digitalgd.com.cn>
2022-06-29 21:25:33 +0800

    feat: 🎸 [messages.ts] add sendMessage api

--- app/api/messages.ts
+++ app/api/messages.ts
@@ -2,9 +2,18 @@ import client from './client'
 
 const messages = '/messages'
 
+type Message = {
+  listingId: number
+  message: string
+}
+
 // 获取消息列表
 const getMessages = () => client.get(messages)
 
+// 发送消息
+const sendMessage = (message: Message) => client.post(messages, message)
+
 export default {
-  getMessages
+  getMessages,
+  sendMessage
 }
